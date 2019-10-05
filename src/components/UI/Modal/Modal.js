7e0e047cb07a6d6dd7dfeb1e29affa8b175be7e6
Nedim Kulovac <nedim.kulovac@gmail.com>
2019-10-05 23:51:20 +0200

    Layout component moved to containers/hoc folder. In hoc folder created Auxiliary folder and Auxiliary.js moved to it.

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 
 import classes from './Modal.css';
-import Auxilary from '../../../hoc/Auxiliary';
+import Auxilary from '../../../hoc/Auxiliary/Auxiliary';
 import Backdrop from '../Backdrop/Backdrop';
 
 class Modal extends Component {
--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 
-import Aux from '../../hoc/Auxiliary';
+import Aux from '../Auxiliary/Auxiliary';
 import classes from './Layout.css';
 import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
-import SideDrawer from '../Navigation/SideDrower/SideDrawer';
+import SideDrawer from '../../components/Navigation/SideDrower/SideDrawer';
 
 class Layout extends Component {
 
