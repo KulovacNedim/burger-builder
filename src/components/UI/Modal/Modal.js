fb8e129cad4dac9aa1a16681ffa95ebc8747ab4e
Nedim Kulovac <nedim.kulovac@gmail.com>
2019-10-05 23:41:57 +0200

    Percormance improved in the way that Modal component is turned into class-component and shouldComponentUpadate method implement. Rerendereng of OrderSummary component is happening only if props.show is changed.

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,21 +1,33 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import classes from './Modal.css';
 import Auxilary from '../../../hoc/Auxiliary';
 import Backdrop from '../Backdrop/Backdrop';
 
-const modal = (props) => (
-    <Auxilary>
-        <Backdrop show={props.show} clicked={props.modalClosed}/>
-        <div
-            className={classes.Modal}
-            style={{
-                transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-                opacity: props.show ? '1' : '0'
-            }}>
-            {props.children}
-        </div>
-    </Auxilary>
-);
+class Modal extends Component {
+    shouldComponentUpdate(nextProps, nextState, nextContext) {
+        return nextProps.show !== this.props.show;
+    }
 
-export default modal;
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        console.log('[Modal] did update');
+    }
+
+    render() {
+        return (
+            <Auxilary>
+                <Backdrop show={this.props.show} clicked={this.props.modalClosed}/>
+                <div
+                    className={classes.Modal}
+                    style={{
+                        transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                        opacity: this.props.show ? '1' : '0'
+                    }}>
+                    {this.props.children}
+                </div>
+            </Auxilary>
+        );
+    }
+}
+
+export default Modal;
