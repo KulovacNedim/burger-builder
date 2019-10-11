32475c583dc47c17614c2c9a2f39bc03c8d4609f
Nedim Kulovac <nedim.kulovac@gmail.com>
2019-10-11 15:26:31 +0200

    burgerBuilder actions for adding and removing ingredients are handled throw redux now

--- src/index.js
+++ src/index.js
@@ -7,9 +7,9 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 
 import { BrowserRouter } from 'react-router-dom';
-import burgerBuilder from './store/reducers/burgerBuilder';
+import burgerBuilderReducer from './store/reducers/burgerBuilder';
 
-const store = createStore(burgerBuilder, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const store = createStore(burgerBuilderReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 const app = (
     <Provider store={store}>
