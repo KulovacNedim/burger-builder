60907bc1cd8790c089dca83d722275b6003d604c
Nedim Kulovac <nedim.kulovac@gmail.com>
2019-10-11 15:11:38 +0200

    Folder restructured and new files to improve redux usage in application.

--- src/index.js
+++ src/index.js
@@ -7,9 +7,9 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 
 import { BrowserRouter } from 'react-router-dom';
-import reducer from '../src/store/reducer';
+import burgerBuilder from './store/reducers/burgerBuilder';
 
-const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const store = createStore(burgerBuilder, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 const app = (
     <Provider store={store}>
